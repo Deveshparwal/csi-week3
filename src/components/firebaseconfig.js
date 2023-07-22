34f5020773f255d706044f1ea2808cd01608de7a
Deveshparwal <84442213+Deveshparwal@users.noreply.github.com>
2023-07-22 22:28:25 +0530

    Add files via upload

---
+++ src/components/firebaseconfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBMGOsxkVqJHiwwMmI5iSuj2RVZvOUf38U",
+  authDomain: "csi-week3.firebaseapp.com",
+  projectId: "csi-week3",
+  storageBucket: "csi-week3.appspot.com",
+  messagingSenderId: "493463058789",
+  appId: "1:493463058789:web:72896aa1d0d0f40d03c98c",
+  measurementId: "G-J6Y7GMRLL2"
+  };
+export default firebaseConfig;
